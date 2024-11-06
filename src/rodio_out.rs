c1b74a8bc36775781802891f141e168ebaa4c008
Geoff Clements <geoff@electron.me.uk>
2024-11-06 21:25:59 +0000

    Basic DecoderSource implemented

--- src/rodio_out.rs
+++ src/rodio_out.rs
@@ -2,10 +2,69 @@ use std::sync::{Arc, Mutex};
 
 use anyhow;
 use crossbeam::channel::Sender;
-use rodio::{cpal::traits::HostTrait, DeviceTrait};
+use rodio::{cpal::traits::HostTrait, DeviceTrait, Source};
 use slimproto::status::StatusData;
 
 use crate::{decode::Decoder, PlayerMsg, StreamParams};
+
+pub struct DecoderSource {
+    decoder: Decoder,
+    frame: Vec<f32>,
+    idx: usize,
+    volume: Arc<Mutex<Vec<f32>>>,
+}
+
+impl DecoderSource {
+    fn new(decoder: Decoder, volume: Arc<Mutex<Vec<f32>>>) -> Self {
+        DecoderSource {
+            decoder,
+            frame: Vec::with_capacity(8 * 1024),
+            idx: 0,
+            volume: volume.clone(),
+        }
+    }
+}
+
+impl Source for DecoderSource {
+    fn current_frame_len(&self) -> Option<usize> {
+        match self.frame.len() {
+            0 => None,
+            len => Some(len),
+        }
+    }
+
+    fn channels(&self) -> u16 {
+        self.decoder.channels() as u16
+    }
+
+    fn sample_rate(&self) -> u32 {
+        self.decoder.sample_rate()
+    }
+
+    fn total_duration(&self) -> Option<std::time::Duration> {
+        None
+    }
+}
+
+impl Iterator for DecoderSource {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.frame.len() == 0 {
+            self.decoder
+                .fill_sample_buffer::<f32>(&mut self.frame, None, self.volume.clone())
+                .ok()?;
+            self.idx = 0;
+        }
+
+        let ret = match self.frame.len() {
+            0 => None,
+            _ => Some(self.frame[self.idx]),
+        };
+        self.idx += 1;
+        ret
+    }
+}
 
 pub struct AudioOutput;
 

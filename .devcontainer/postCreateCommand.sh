1fe30ea4b7a9777ee8633a96962d2ad8bc8927d4
Geoff Clements <geoff@electron.me.uk>
2026-02-12 12:19:48 +0000

    Rename container and mkdir before linking pipewire pipe

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,16 @@
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
-    libpulse-dev \
-    libasound2-dev \
-    libdbus-1-dev \
-    pkg-config \
-    libpipewire-0.3-dev \
-    libspa-0.2-dev \
-    libclang-dev \
-    pipewire
+  libpulse-dev \
+  libasound2-dev \
+  libdbus-1-dev \
+  pkg-config \
+  libpipewire-0.3-dev \
+  libspa-0.2-dev \
+  libclang-dev \
+  pipewire
 
 cargo install cargo-deb
 
+mkdir -p /tmp/user/1000/
 ln -s /run/user/1000/pipewire-0 /tmp/user/1000/pipewire-0
+
